cfaaf472f155eabc2071c8929ef7e0f2d7a90da2
Josh Sherman <joshua.v.sherman@gmail.com>
2017-04-20 11:30:25 -0400

    books are working again

--- model/book/book-schema.js
+++ model/book/book-schema.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
-
 const bookSchema = new Schema({
   title: { type: String, required: true },
   type: { type: String, required: true },
@@ -10,12 +9,14 @@ const bookSchema = new Schema({
   dateOfPub: { type: Number, required: false },
   url: { type: String, required: false },
   // isCheckedOut: { type: Number, required: false },
-// isbn is either the GE number or the orderable number from a bookstore
+  // isbn is either the GE number or the orderable number from a bookstore
   isbn: { type: String, required: false },
   siteLocation: { type: String, required: false },
   numberOfCopies: { type: Number, required: false },
-  comments: { type: String, required: false }
+  access: { type: String, required: false },
+  comments: { type: String, required: false },
+  checkedOutBy: { type: String, required: false },
+  checkedOutByName: { type: String, required: false }
 });
-
 
 module.exports = mongoose.model('Book', bookSchema);
