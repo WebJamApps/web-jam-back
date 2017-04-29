fe1d9ea926d5f4d2e569b196981da3da14d307fb
M. Adam Kendall <michael.kendall@gsa.gov>
2017-04-29 15:45:02 -0400

    Use es6 classes with static functions

--- auth/authUtils.js
+++ auth/authUtils.js
@@ -1,39 +1,42 @@
 
 const moment = require('moment');
-// let is needed for rewire :(
+// let is needed for rewire support
 let jwt = require('jwt-simple');  // eslint-disable-line prefer-const
 const config = require('../config');
 
-exports.createJWT = function(user) {
-    const payload = {
-        sub: user._id,
-        iat: moment().unix(),
-        exp: moment().add(14, 'days').unix()
-    };
-    return jwt.encode(payload, config.hashString);
-};
-
-exports.handleError = function(res, err) {
-    return res.send(400, err);
-};
-
-
-exports.ensureAuthenticated = function(req, res, next) {
-  if (!req.headers.authorization) {
-    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
-  }
-  const token = req.headers.authorization.split(' ')[1];
-
-  let payload = null;
-  try {
-    payload = jwt.decode(token, process.env.HashString);
-  } catch (err) {
-    return res.status(401).send({ message: err.message });
+class AuthUtils {
+  static createJWT(user) {
+      const payload = {
+          sub: user._id,
+          iat: moment().unix(),
+          exp: moment().add(14, 'days').unix()
+      };
+      return jwt.encode(payload, config.hashString);
   }
 
-  if (payload.exp <= moment().unix()) {
-    return res.status(401).send({ message: 'Token has expired' });
+  static handleError(res, err) {
+      return res.send(400, err);
   }
-  req.user = payload.sub;
-  next();
-};
+
+  static ensureAuthenticated(req, res, next) {
+    if (!req.headers.authorization) {
+      return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+
+    let payload = null;
+    try {
+      payload = jwt.decode(token, process.env.HashString);
+    } catch (err) {
+      return res.status(401).send({ message: err.message });
+    }
+
+    if (payload.exp <= moment().unix()) {
+      return res.status(401).send({ message: 'Token has expired' });
+    }
+    req.user = payload.sub;
+    next();
+  }
+}
+
+module.exports = AuthUtils;
