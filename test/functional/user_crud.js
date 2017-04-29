e310b91d2469b44162e3cc5d4e9339475930af45
M. Adam Kendall <michael.kendall@gsa.gov>
2017-04-29 15:37:29 -0400

    Completely clean up all linting errors. Fixes #76

---
+++ test/functional/user_crud.js
@@ -0,0 +1,121 @@
+const User1 = require('../../model/user/user-schema');
+const authUtils = require('../../auth/authUtils');
+describe('functional test Create User',  () => {
+  beforeEach((done) => {
+    User1.collection.drop();
+    User1.ensureIndexes();
+    mockgoose(mongoose).then(() => {
+      allowedUrl = JSON.parse(process.env.AllowUrl).urls[0];
+      global.server = require('../../index'); // eslint-disable-line global-require
+      done();
+    });
+  });
+
+  it('should create a new user', (done) => {
+    const User = new User1();
+    User.name = 'foo';
+    User.email = 'foo@example.com';
+    User.save((err) => {
+      const id = User._id;
+      expect(id).to.not.be.null; // eslint-disable-line no-unused-expressions
+      done();
+    });
+  });
+
+  it('should not update a user when using a ID that does not exist', (done) => {
+    const Uid = '587298a376d5036c68b6ef12';
+    chai.request(server)
+    .put('/user/' + Uid)
+    .set({ origin: allowedUrl })
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
+    .send({ userType: 'coolGuy' })
+    .end((err, res) => {
+      expect(res).to.have.status(404);
+      done();
+    });
+  });
+
+  it('should modify a user', (done) => {
+    const User = new User1();
+    User.name = 'foo';
+    User.email = 'foo2@example.com';
+    User.save();
+    chai.request(server)
+    .put('/user/' + User.id)
+    .set({ origin: allowedUrl })
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
+    .send({ name: 'foobar' })
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.nModified > 0);
+      // TODO: Write a GET request to verify that user's name has been changed
+      done();
+    });
+  });
+
+  it('should find a user by id', (done) => {
+    const User = new User1();
+    User.name = 'foo';
+    User.email = 'foo3@example.com';
+    User.save();
+    chai.request(server)
+    .get('/user/' + User._id)
+    .set({ origin: allowedUrl })
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+  it('should NOT find a user by id', (done) => {
+    const id = '587298a376d5036c68b6ef12';
+    chai.request(server)
+    .get('/user/' + id)
+    .set({ origin: allowedUrl })
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
+    .end((err, res) => {
+      expect(res).to.have.status(404);
+      done();
+    });
+  });
+
+  it('should return an error in update() when nothing was updated', (done) => {
+    const Uid = '587298a376d5036c68b6ef12';
+    chai.request(server)
+    .put('/user/' + Uid)
+    .set({ origin: allowedUrl })
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
+    .send({ alien: 'yes' })
+    .end((err, res) => {
+      expect(err).to.be.an('error');
+      done();
+    });
+  });
+
+  it('should return 404 error when Id not valid on update', (done) => {
+    const Uid = '5872';
+    chai.request(server)
+    .put('/user/' + Uid)
+    .set({ origin: allowedUrl })
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
+    .send({ alien: 'yes' })
+    .end((err, res) => {
+      expect(err).to.be.an('error');
+      expect(res).to.have.status(400);
+      done();
+    });
+  });
+
+  it('should throw an error in findById()', (done) => {
+    const id = 'TYgsfn';
+    chai.request(server)
+    .get('/user/' + id)
+    .set({ origin: allowedUrl })
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
+    .end((err, res) => {
+      expect(err).to.be.an('error');
+      done();
+    });
+  });
+});
---
+++ test/unit/google_unit.js
@@ -0,0 +1,80 @@
+const rewire = require('rewire');
+const google = rewire('../../auth/google');
+
+describe('The Unit Test for Google Module', () => {
+  let User;
+  let request;
+  let authUtils;
+  let revertUser;
+  let revertRequest;
+  let revertAuthUtils;
+
+  beforeEach(() => {
+    User = sinon.stub();
+    User.findOne = sinon.stub();
+    revertUser = google.__set__('User', User);
+
+    request = { get: sinon.stub(), post: sinon.stub() };
+    revertRequest = google.__set__('request', request);
+
+    authUtils = { createJWT: sinon.stub() };
+    revertAuthUtils = google.__set__('authUtils', authUtils);
+  });
+
+  afterEach(() => {
+    revertUser();
+    revertRequest();
+    revertAuthUtils();
+  });
+
+  it('should authenticate', (done) => {
+    const token = { access_token: 'access_token' };
+    request.post.yields(null, {}, token);
+
+    const profile = { email: 'email' };
+    request.get.yields(null, {}, profile);
+
+    const existingUser = { _id: 'someid' };
+    User.findOne.yields(null, existingUser);
+
+    const lastToken = 'lastToken';
+    authUtils.createJWT.returns(lastToken);
+    const req = { body: {} };
+    const res = {
+      send: (msg) => {
+        expect(msg).to.have.property('token');
+        expect(msg.token).to.equal(lastToken);
+        done();
+      }
+    };
+
+    google.authenticate(req, res);
+  });
+
+  it('should create a new user and authenticate', (done) => {
+    const token = { access_token: 'access_token' };
+    request.post.yields(null, {}, token);
+
+    const profile = { email: 'email' };
+    request.get.yields(null, {}, profile);
+
+    User.findOne.yields(null, null);
+    const newUser = { save: sinon.stub() };
+    User.returns(newUser);
+    newUser.save.yields(null);
+
+    const lastToken = 'lastToken';
+    authUtils.createJWT.returns(lastToken);
+
+    const req = { body: {} };
+    const res = {
+      send: (msg) => {
+        expect(msg).to.have.property('token');
+        expect(msg.token).to.equal(lastToken);
+        done();
+      }
+    };
+
+    google.authenticate(req, res);
+  });
+});
