9d3e1010db144e3c658b4fa7afb3b22f44fb2dc8
M. Adam Kendall <michael.kendall@gsa.gov>
2017-04-29 16:10:47 -0400

    Clean up editorconfig issues

--- model/user/user-router.js
+++ model/user/user-router.js
@@ -8,8 +8,8 @@ const router = new Router();
 //   .post((...args) => controller.create(...args));
 
 router.route('/:id')
-   .put((...args) => controller.update(...args))
+  .put((...args) => controller.update(...args))
   .get((...args) => controller.findById(...args));
-//   .delete((...args) => controller.remove(...args));
+  // .delete((...args) => controller.remove(...args));
 
 module.exports = router;
