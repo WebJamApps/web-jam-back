4c1ebdfba37edc6e792dec301123cf674e163c4c
Josh Sherman <joshua.v.sherman@gmail.com>
2017-05-12 10:41:22 -0400

    fixed some tests and commented out unused code

--- test/functional/book_crud.js
+++ test/functional/book_crud.js
@@ -1,5 +1,5 @@
 const Book1 = require('../../model/book/book-schema');
-// const authUtils = require('../../auth/authUtils');
+const authUtils = require('../../auth/authUtils');
 
 describe('The library feature',  () => {
   beforeEach((done) => {
@@ -14,16 +14,16 @@ describe('The library feature',  () => {
   });
   it('should create a new book', (done) => {
     chai.request(server)
-    .post('/book/')
+    .post('/book/create')
     .set({ origin: allowedUrl })
-    .set('authorization', 'Bearer ')
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
     .send({ title: 'foobar', type: 'book' })
     .end((err, res) => {
       // expect(res).to.have.status(201);
       done();
     });
   });
-
+  
   it('should raise error when no books are found', (done) => {
     chai.request(server)
     .get('/book/getall')
@@ -35,7 +35,7 @@ describe('The library feature',  () => {
       done();
     });
   });
-
+  
   it('should return all books', (done) => {
     const Book = new Book1();
     Book.title = 'foo2book';
@@ -51,19 +51,19 @@ describe('The library feature',  () => {
       });
     });
   });
-
+  
   it('should post an array of new books', (done) => {
     chai.request(server)
-    .post('/book/')
+    .post('/book/create')
     .set({ origin: allowedUrl })
-    .set('authorization', 'Bearer ')
+    .set('authorization', 'Bearer ' + authUtils.createJWT('foo2@example.com'))
     .send([{ title: 'foobar', type: 'book' }, { title: 'JFK', type: 'PDF' }])
     .end((err, res) => {
       // expect(res).to.have.status(201);
       done();
     });
   });
-
+  
   // when you call with a non-existent path, be sure to get a 404.
   it('should pass for the error', (done) => {
     chai.request(server)
@@ -75,7 +75,7 @@ describe('The library feature',  () => {
       done();
     });
   });
-
+  
   // it('should respond with error on find a book', (done) => {
   //   chai.request(server)
   //   .get('/book/find/one')
