1c36d64c6d50eefac03805b3f6169b6aa93672cd
Josh Sherman <joshua.v.sherman@gmail.com>
2017-05-31 13:33:55 -0400

    added volunteering attributes to user and removed volunteer model

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const router = new Router();
 const user  = require('./model/user/user-router');
 const book = require('./model/book/book-router');
 const charity = require('./model/charity/charity-router');
-const volunteer = require('./model/volunteer/volunteer-router');
 const auth = require('./auth');
 const authUtils = require('./auth/authUtils');
 
@@ -13,5 +12,4 @@ module.exports = function(app) {
     router.use('/user', authUtils.ensureAuthenticated, user);
     router.use('/book', book);
     router.use('/charity', authUtils.ensureAuthenticated, charity);
-      router.use('/volunteer', authUtils.ensureAuthenticated, volunteer);
 };
