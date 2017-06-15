8d1e874f50dcef65f6ae08e6fc9aead4ec5775c3
Josh Sherman <joshua.v.sherman@gmail.com>
2017-06-15 16:12:28 -0400

    remove all books

--- model/book/book-controller.js
+++ model/book/book-controller.js
@@ -5,11 +5,21 @@ class BookController extends Controller {
 
   find(req, res, next) {
     return this.model.find(req.query)
-      .then((collection) => {
-        console.log(collection.length);
-        if (collection.length) return res.status(200).json(collection);
-        return next(new Error('Bookshelf Empty'));
-      });
+    .then((collection) => {
+      console.log(collection.length);
+      if (collection.length) return res.status(200).json(collection);
+      return next(new Error('Bookshelf Empty'));
+    });
+  }
+
+  remove(req, res, next) {
+    return this.model.remove(req.query);
+    // .then((doc) => {
+    //   if (!doc) {
+    //     return res.status(404).send({ message: 'Delete id is invalid' });
+    //   }
+    //   return res.status(204).end();
+    // });
   }
   // findByTitle(req, res, next) {
   //   let bar = this.model;
