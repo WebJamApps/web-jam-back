bd40d2e8fbd6c60a99438b9046f4a37817a7df32
Josh Sherman <joshua.v.sherman@gmail.com>
2017-06-26 19:43:07 -0400

    get user by email

--- model/user/user-controller.js
+++ model/user/user-controller.js
@@ -2,6 +2,16 @@ const Controller = require('../../lib/controller');
 const userModel  = require('./user-facade');
 
 
-class UserController extends Controller {}
+class UserController extends Controller {
+    find(req, res, next) {
+      console.log('this is the user email: ' + req.body.email);
+      return this.model.find({ email:req.body.email })
+      .then((collection) => {
+        console.log(collection.length);
+        return res.status(200).json(collection);
+      });
+    }
+
+}
 
 module.exports = new UserController(userModel);
