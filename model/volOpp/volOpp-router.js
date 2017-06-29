06c7394a1094fb915017245968cbbf0f20e0ca32
Joshua Sherman <joshua.v.sherman@gmail.com>
2017-06-29 12:21:54 -0400

    create vol opp event

--- model/volOpp/volOpp-router.js
+++ model/volOpp/volOpp-router.js
@@ -5,10 +5,10 @@ const router = new Router();
 router.route('/create')
 .post((...args) => controller.create(...args));
 
-router.route('/:id')
+// router.route('/:id')
 // the id for this get request is the user id
-.get((...args) => controller.find(...args))
-.delete((...args) => controller.findByIdAndRemove(...args))
-.put((...args) => controller.update(...args));
+// .get((...args) => controller.find(...args))
+// .delete((...args) => controller.findByIdAndRemove(...args))
+// .put((...args) => controller.update(...args));
 
 module.exports = router;
