8ab2a02cd6bd64c87742bd6eeafe0db0d2f6a9e1
Joshua Sherman <joshua.v.sherman@gmail.com>
2017-06-29 10:45:26 -0400

    user zipcode is a string, also updated node version

---
+++ model/user/user-schema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  userPhone: { type: Number, required: false },
+  userType: { type: String, required: false },
+  userCity: { type: String, required: false },
+  userZip: { type: String, required: false },
+  userDetails:{ type: String, required: false },
+  volTravelDistMiles: { type: Number, required: false },
+  volCauses: { type: [String], required: false },
+  volTalents: { type: [String], required: false },
+  volWorkPrefs: { type: [String], required: false },
+  volCauseOther:{ type: String, required: false },
+  volTalentOther:{ type: String, required: false },
+  volWorkOther:{ type: String, required: false }
+});
+
+
+module.exports = mongoose.model('User', userSchema);
