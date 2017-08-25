992ac86bbda2e46df859427550704241abcfa36e
“Joshua <joshua.v.sherman@gmail.com>
2017-08-25 12:01:41 -0400

    more attributes for charities and events

--- model/charity/charity-schema.js
+++ model/charity/charity-schema.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const charitySchema = new Schema({
   charityName: { type: String, required: true },
+  charityStreet: { type: String, required: false },
   charityCity: { type: String, required: false },
   charityState: { type: String, required: false },
-  charityZipCode: { type: String, required: true },
-  charityPhoneNumber: { type: Number, required: false },
+  charityZipCode: { type: String, required: false },
+  charityPhoneNumber: { type: String, required: false },
   charityEmail: { type: String, required: false },
   charityTypes: { type: [String], required: false },
   charityManagers: { type: [String], required: false },
