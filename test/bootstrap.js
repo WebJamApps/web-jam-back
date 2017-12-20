7a9aba17c15a064c05e465e95f0e477a7ada16bd
Joshua V Sherman <joshua.v.sherman@gmail.com>
2017-12-20 10:42:14 -0500

    refactoring

---
+++ test/bootstrap.js
@@ -0,0 +1,11 @@
+global.bluebird = require('bluebird');
+global.chai = require('chai');
+global.chaiHttp = require('chai-http');
+global.sinon = require('sinon');
+global.mongoose = require('mongoose');
+global.mockgoose = require('mockgoose');
+mongoose.Promise = bluebird;
+process.env.NODE_ENV = 'dev';
+process.env.MONGO_DB_URI = 'localhost';
+global.expect = chai.expect;
+chai.use(chaiHttp);
