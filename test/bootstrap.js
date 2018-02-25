55d73f1ea4803e76c15981df52b2380725c55e1d
Joshua V Sherman <joshua.v.sherman@gmail.com>
2018-02-25 14:07:54 -0500

    no more sending emails when testing

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -5,7 +5,7 @@ global.sinon = require('sinon');
 global.mongoose = require('mongoose');
 global.mockgoose = require('mockgoose');
 mongoose.Promise = bluebird;
-process.env.NODE_ENV = 'dev';
+process.env.NODE_ENV = 'test';
 process.env.MONGO_DB_URI = 'localhost';
 global.expect = chai.expect;
 chai.use(chaiHttp);
