1d081cabadf2e97f30ff954c4147bc4d71d2bab3
“Joshua <joshua.v.sherman@gmail.com>
2018-10-03 15:18:29 -0400

    removes todo since it is in a github issue now

--- model/user/user-controller.js
+++ model/user/user-controller.js
@@ -2,7 +2,7 @@ const Controller = require('../../lib/controller');
 const userModel = require('./user-facade');
 
 class UserController extends Controller {
-  find(req, res) { // TODO make this a get with query and also update frontend fetch request
+  find(req, res) {
     return this.model.find({ email: req.body.email })
       .then(collection => res.status(200).json(collection))
       .catch(err => res.status(500).json({ message: 'failed to find user by email', error: err }));
