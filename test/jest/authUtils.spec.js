f13e568df427b85e65dc84b20a70b3662bff127a
JoshuaVSherman <joshua.v.sherman@gmail.com>
2019-08-17 17:13:55 -0400

    fixes from upgrade

---
+++ test/jest/authUtils.spec.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-useless-catch */
+const EventEmitter = require('events');
+const AuthUtils = require('../../auth/authUtils');
+
+EventEmitter.defaultMaxListeners = Infinity;
+describe('the authUtils', () => {
+  it('validates email syntax', async () => {
+    try {
+      const cb = await AuthUtils.checkEmailSyntax({ body: { changeemail: 'j@jb.com' } });
+      expect(cb).toBe(true);
+    } catch (e) { throw e; }
+  });
+  it('validates email syntax and returns error', async () => {
+    try {
+      await AuthUtils.checkEmailSyntax({ body: { changeemail: 'bogas' } });
+    } catch (e) {
+      expect(e.message).toBe('email address is not a valid format');
+    }
+  });
+});
