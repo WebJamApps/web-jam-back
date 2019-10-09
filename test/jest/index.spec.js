8ee0d260255a4d82a9ffe8aa8dc9212dfe1be6c6
Joshua Sherman <jsherman@valcom.com>
2019-10-09 08:07:08 -0400

    index unit tests

--- test/jest/index.spec.js
+++ test/jest/index.spec.js
@@ -1,41 +1,30 @@
+require('dotenv').config();
+const request = require('supertest');
+
+const AllowUrl = JSON.parse(process.env.AllowUrl);
+
 describe('Index test', () => {
-  // let allowedUrl, server;
-  // beforeEach((done) => {
-  //   allowedUrl = JSON.parse(process.env.AllowUrl).urls[0];// eslint-disable-line prefer-destructuring
-  //   server = require('../../index'); // eslint-disable-line global-require
-  //   done();
-  // });
+  let allowedUrl, server, r;
+  beforeEach((done) => {
+    [allowedUrl] = AllowUrl.urls;
+    server = require('../../index'); // eslint-disable-line global-require
+    done();
+  });
   it('does nothing', (done) => {
     done();
   });
-  // it('should return status 200 when use -> app.get', (done) => {
-  //   chai.request(server)
-  //     .get('/anyrul')
-  //     .set({ origin: allowedUrl })
-  //     .set('authorization', 'Bearer ')
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(200);
-  //       done();
-  //     });
-  // });
-  // it('should return status 200 when use -> app.get to /music/buymusic', (done) => {
-  //   chai.request(server)
-  //     .get('/music/buymusic')
-  //     .set({ origin: allowedUrl })
-  //     .set('authorization', 'Bearer ')
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(200);
-  //       done();
-  //     });
-  // });
-  // it('should return status 200 when use -> app.get to /shop/inventory', (done) => {
-  //   chai.request(server)
-  //     .get('/shop/inventory')
-  //     .set({ origin: allowedUrl })
-  //     .set('authorization', 'Bearer ')
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(200);
-  //       done();
-  //     });
-  // });
+  it('should return status 200 when use -> app.get', async () => {
+    r = await request(server)
+      .get('/anyrul')
+      .set({ origin: allowedUrl })
+      .set('authorization', 'Bearer ');
+    expect(r.status).toBe(200);
+  });
+  it('should return 500 error', async () => {
+    r = await request(server)
+      .delete('/bogus')
+      .set({ origin: 'bogus' })
+      .set('authorization', 'Bearer ');
+    expect(r.status).toBe(500);
+  });
 });
