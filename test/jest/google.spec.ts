dc5d5bdaaeb87d145a695017c98fd0ac2a65e0a2
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-07-20 10:14:37 -0400

    finishes initial typescript conversion

--- test/jest/google.spec.ts
+++ test/jest/google.spec.ts
@@ -3,14 +3,14 @@ import google from '../../src/auth/google';
 
 describe('The Unit Test for Google Module', () => {
   it('authenticates returns a 401 error', async () => {
-    const req: any = { body: { code: 'whatever', clientId: '123', redirectUrl: 'http://whatever.com' } };
+    const req = { body: { code: 'whatever', clientId: '123', redirectUri: 'http://whatever.com' } };
     await expect(google.authenticate(req))
       .rejects.toThrow('Unauthorized');
   });
   it('catches error on fetching a profile', async () => {
     const sa: any = superagent;
     sa.post = jest.fn(() => ({ type: () => ({ send: () => ({ set: () => Promise.resolve({}) }) }) }));
-    const req: any = { body: { code: 'whatever', clientId: '123', redirectUrl: 'http://whatever.com' } };
+    const req = { body: { code: 'whatever', clientId: '123', redirectUri: 'http://whatever.com' } };
     await expect(google.authenticate(req))
       .rejects.toThrow('Cannot read property \'access_token\' of undefined');
   });
@@ -18,8 +18,16 @@ describe('The Unit Test for Google Module', () => {
     const sa: any = superagent;
     sa.post = jest.fn(() => ({ type: () => ({ send: () => ({ set: () => Promise.resolve({ body: { access_token: 'booya' } }) }) }) }));
     sa.get = jest.fn(() => ({ set: () => Promise.resolve(null) }));
-    const req: any = { body: { code: 'whatever', clientId: '123', redirectUrl: 'http://whatever.com' } };
+    const req = { body: { code: 'whatever', clientId: '123', redirectUri: 'http://whatever.com' } };
     await expect(google.authenticate(req))
       .rejects.toThrow('failed to retrieve user profile from Google');
+  });
+  it('uses http to when localhost auth', async () => {
+    const sa: any = superagent;
+    sa.post = jest.fn(() => ({ type: () => ({ send: () => ({ set: () => Promise.resolve({ body: { access_token: 'booya' } }) }) }) }));
+    sa.get = jest.fn(() => ({ set: () => Promise.resolve({ body: { emailAddresses: ['me@me.com'] } }) }));
+    const req = { body: { code: 'whatever', clientId: '123', redirectUri: 'https://localhost.com' } };
+    const res = await google.authenticate(req);
+    expect(res.emailAddresses[0]).toBe('me@me.com');
   });
 });
