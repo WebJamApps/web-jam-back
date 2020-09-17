acd6c5835c092ebd1870913a38cbb6cce13df94a
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-09-17 08:17:16 -0400

    WIP typescript learning

--- src/ReadCSV/index.ts
+++ src/ReadCSV/index.ts
@@ -1,9 +1,14 @@
 import csvtojson from 'csvtojson';
+import Debug from 'debug';
 
+const debug = Debug('web-jam-back:ReadCSV');
 interface IReadCSV {
+  date: string | Date;
   homeTeam: string;
   winner: string;
   awayTeam: string;
+  homeScore: number;
+  awayScore: number;
 }
 
 class ReadCSV {
@@ -24,27 +29,48 @@ class ReadCSV {
     }
     let manWins = 0;
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const match of this.soccerMatches) {
+    this.soccerMatches.map((match) => {
       if (match.homeTeam === 'Man United' && match.winner === MatchResult.HomeWin) {
-        // eslint-disable-next-line no-plusplus
-        manWins++;
+        manWins += 1;
       } else if (match.awayTeam === 'Man United' && match.winner === MatchResult.AwayWin) {
-        // eslint-disable-next-line no-plusplus
-        manWins++;
+        manWins += 1;
       }
-    }
+      return match;
+    });
+    debug(this.soccerMatches);
     return `Man United won ${manWins} games`;
+  }
+
+  convertDates():void{
+    this.soccerMatches = this.soccerMatches.map((m) => {
+      const match = m;
+      if (typeof match.date !== 'string') return match;
+      const tmpDate:string = match.date;
+      const tmpDateArr = tmpDate.split('/');
+      match.date = new Date(Number(tmpDateArr[2]), Number(tmpDate[1]) - 1, Number(tmpDate[0]));
+      return match;
+    });
+  }
+
+  convertScores():void{
+    this.soccerMatches = this.soccerMatches.map((m) => {
+      const match = m;
+      match.homeScore = Number(match.homeScore);
+      match.awayScore = Number(match.awayScore);
+      return match;
+    });
   }
 
   async run(): Promise<string> {
     try {
       this.soccerMatches = await this.csvtojson({
         noheader: true,
-        headers: ['data', 'homeTeam', 'awayTeam', 'homeScore', 'awayScore', 'winner', 'mvp'],
+        headers: ['date', 'homeTeam', 'awayTeam', 'homeScore', 'awayScore', 'winner', 'mvp'],
       })
         .fromFile('./src/ReadCSV/football.csv');
     } catch (e) { return `${e.message}`; }
+    this.convertDates();
+    this.convertScores();
     return this.manUnitedWins();
   }
 }
