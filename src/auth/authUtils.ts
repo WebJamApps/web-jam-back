6665155b961cdd47fa49aff44d77ead415706dfa
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-09-24 09:22:11 -0400

    WIP adding access to routes based on userType

--- src/auth/authUtils.ts
+++ src/auth/authUtils.ts
@@ -1,11 +1,31 @@
 import moment from 'moment';
 import jwt from 'jwt-simple';
+import Debug from 'debug';
 import dotenv from 'dotenv';
 import sgMail from '@sendgrid/mail';
+import userModel from '../model/user/user-schema';
 
 dotenv.config();
+const debug = Debug('web-jam-back:authUtils');
+const findUserById = async (req: { user: any; userType: string; baseUrl:string }, 
+  res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): any; new(): any; }; }; }, 
+  next: () => any):Promise<any> => {
+  let myUser:any;
+  try { myUser = await userModel.findById(req.user).lean().exec(); } catch (e) {
+    return res.status(500).json({ message: `token does not match any existing user, ${e.message}` });
+  }
+  req.userType = myUser ? myUser.userType : 'none';
+  debug(req.userType);
+  debug(req.baseUrl);
+  const authRoles:any = JSON.parse(process.env.AUTH_ROLES || '{}');
+  const route = req.baseUrl.split('/')[1];
+  // eslint-disable-next-line security/detect-object-injection
+  const rolesArr = authRoles[route];
+  if (rolesArr.indexOf(req.userType) === -1) return res.status(401).json({ message: 'The user does not have the permission' });
+  return next();
+};
 class AuthUtils {
-  static createJWT(user: any): any {
+  static createJWT(user: {_id:string}): string {
     const payload = {
       sub: user._id,
       iat: moment().unix(),
@@ -14,19 +34,20 @@ class AuthUtils {
     return jwt.encode(payload, process.env.HashString || /* istanbul ignore next */'');
   }
 
-  static ensureAuthenticated(req: any, res: any, next: any): any {
-    if (!req.headers.authorization) {
-      return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+  static ensureAuthenticated(req: any, res: any, next: any): Promise<any> {
+    if (!req.headers.authorization && !req.headers.Authorization) {
+      return res.status(401).send({ message: 'The request does not have an Authorization header' });
     }
-    const token = req.headers.authorization.split(' ')[1];
-    let payload = { sub: '' };
+    let payload = { sub: '' }, token:string = req.headers.authorization || req.headers.Authorization;
+    // eslint-disable-next-line prefer-destructuring
+    token = token.split(' ')[1];
     try {
       payload = jwt.decode(token, process.env.HashString || /* istanbul ignore next */'');
     } catch (err) {
       return res.status(401).send({ message: err.message });
     }
     req.user = payload.sub;// this is the userId
-    // set the userType by fetching the user by id
+    if (process.env.NODE_ENV !== 'test') return findUserById(req, res, next);
     return next();
   }
 
