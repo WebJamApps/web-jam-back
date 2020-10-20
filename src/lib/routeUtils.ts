230f63ae75d45e6178281792b2b8add8d8db3df0
Rhayven <codingforllamas@gmail.com>
2020-10-20 08:37:12 -0400

    More fixes

--- src/lib/routeUtils.ts
+++ src/lib/routeUtils.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import AuthUtils from '../auth/authUtils';
+import Controller from './controller';
 
-function setRoot(router: Router, controller: any, authUtils: typeof AuthUtils): void {
+function setRoot(router: Router, controller: Controller, authUtils: typeof AuthUtils): void {
   router.route('/')
     .get((req, res) => controller.find(req, res))
     .post(authUtils.ensureAuthenticated, (req, res) => controller.create(req, res))
     .delete(authUtils.ensureAuthenticated, (req, res) => controller.deleteMany(req, res));
 }
-function byId(router: Router, controller: any, authUtils: typeof AuthUtils): void {
+function byId(router: Router, controller: Controller, authUtils: typeof AuthUtils): void {
   router.route('/:id')
     .get(authUtils.ensureAuthenticated, (req, res) => controller.findById(req, res))
     .put(authUtils.ensureAuthenticated, (req, res) => controller.findByIdAndUpdate(req, res))
