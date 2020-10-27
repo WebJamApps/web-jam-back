a6d796645b7d39196b2416ee643bb3b4ae7a7cee
Rhayven <codingforllamas@gmail.com>
2020-10-27 17:58:41 -0400

    Fixes google.ts and reverts authUtils for now

--- src/auth/authUtils.ts
+++ src/auth/authUtils.ts
@@ -8,20 +8,20 @@ import userModel from '../model/user/user-schema';
 dotenv.config();
 const debug = Debug('web-jam-back:authUtils');
 
-const findUserById = async (req: { user: string; userType: string; baseUrl: string },
-  res: { status: (arg0: number) => { json: { (arg0: { message: string; }): void; }; }; },
-  next: () => void): Promise<unknown> => {
-  let myUser: any;
+const findUserById = async (req: { user: any; userType: string; baseUrl:string },
+  res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): any; new(): any; }; }; },
+  next: () => any):Promise<any> => {
+  let myUser:any;
   try { myUser = await userModel.findById(req.user).lean().exec(); } catch (e) {
     return res.status(500).json({ message: `token does not match any existing user, ${e.message}` });
   }
   req.userType = myUser ? myUser.userType : 'none';
   debug(req.userType);
   debug(req.baseUrl);
-  const authRoles: Record<string, string[]> = JSON.parse(process.env.AUTH_ROLES || /* istanbul ignore next */'{}');
+  const authRoles:any = JSON.parse(process.env.AUTH_ROLES || /* istanbul ignore next */'{}');
   const route = req.baseUrl.split('/')[1];
   // eslint-disable-next-line security/detect-object-injection
-  const rolesArr: string[] = authRoles[route] || /* istanbul ignore next */[];
+  const rolesArr: any[] = authRoles[route] || /* istanbul ignore next */[];
   if (rolesArr.length && rolesArr.indexOf(req.userType) === -1) return res.status(401).json({ message: 'The user does not have the permission' });
   return next();
 };
@@ -35,8 +35,7 @@ const createJWT = (user: {_id:string}): string => {
   return jwt.encode(payload, process.env.HashString || /* istanbul ignore next */'');
 };
 
-const ensureAuthenticated = (req: any, res: any,
-  next: () => void): Promise<unknown> => {
+const ensureAuthenticated = (req: any, res: any, next: any): Promise<any> => {
   if (!req.headers.authorization && !req.headers.Authorization) {
     return res.status(401).send({ message: 'The request does not have an Authorization header' });
   }
@@ -73,7 +72,7 @@ const generateCode = (hi: number, low: number): number => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-const checkEmailSyntax = (req: { body: { changeemail: string } }): Promise<boolean> => { // eslint-disable-next-line security/detect-unsafe-regex
+const checkEmailSyntax = (req: any): Promise<boolean> => { // eslint-disable-next-line security/detect-unsafe-regex
   if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(req.body.changeemail)) {
     return Promise.resolve(true);
   }
