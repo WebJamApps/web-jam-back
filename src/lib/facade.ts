53c9f2978bd256183bedcf8ac83422009ae9ca39
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-10-30 08:35:41 -0400

    refactoring continues

--- src/lib/facade.ts
+++ src/lib/facade.ts
@@ -1,20 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type { Model, Document, Query } from 'mongoose';
+import type { Query, Document } from 'mongoose';
 
-interface Ischema {
-  modelName:string;
-  create:(...args:any)=>any;
-  deleteMany:(...args:any)=>any;
-  findOneAndUpdate:(...args:any)=>any;
-  findByIdAndUpdate:(...args:any)=>any;
-  findById:(...args:any)=>any;
-  findByIdAndRemove:(...args:any)=>any;
-  find:(...args:any)=>any;
-  findOne:(...args:any)=>any;
-}
 enum Method {
   find = 'find', findOne = 'findOne'
 }
+interface Ischema {
+  modelName:string;
+  create:(...args:[Record<string, unknown>])=>Promise<Document>;
+  deleteMany:(...args:[Query<Record<string, unknown>>])=>({lean:()=>({exec:()=>any})});
+  findOneAndUpdate:(...args:[Record<string, unknown>, Record<string, unknown>, {new:boolean}])=>({lean:()=>({exec:()=>Promise<Document | null>})});
+  findByIdAndUpdate:(...args:[string, Record<string, unknown>, {new:boolean}])=>({lean:()=>({exec:()=>Promise<Document | null>})});
+  findById:(...args:[string])=>({lean:()=>({exec:()=>Promise<Document | null>})});
+  findByIdAndRemove:(...args:[string])=>({lean:()=>({exec:()=>Promise<Document | null >})});
+  find:(...args:[Query<Record<string, unknown>>])=>({lean:()=>({exec:()=>Promise<any>})});
+  findOne:(...args:[Query<Record<string, unknown>>])=>({lean:()=>({exec:()=>Promise<Document | null>})});
+}
+
 class Facade {
   Schema: Ischema;
 
@@ -22,21 +23,21 @@ class Facade {
     this.Schema = Schema;
   }
 
-  create(input: Record<string, unknown>): Promise<any> { return this.Schema.create(input); }
+  create(input: Record<string, unknown>): Promise<Document> { return this.Schema.create(input); }
 
-  async f(query: Query<any>, method: Method):Promise<any> {
+  async f(query: Query<Record<string, unknown>>, method: Method):Promise<any| Document | null> {
     let result;// eslint-disable-next-line security/detect-object-injection
     try { result = await this.Schema[method](query).lean().exec(); } catch (e) { return Promise.reject(e); }
     return result;
   }
 
-  find(query: Query<any>): Promise<any> { return this.f(query, Method.find); }
+  find(query: Query<Record<string, unknown>>): Promise<any> { return this.f(query, Method.find); }
 
-  findOne(query: Query<any>): Promise<any> { return this.f(query, Method.findOne); }
+  findOne(query: Query<Record<string, unknown>>): Promise<Document | null> { return this.f(query, Method.findOne); }
 
-  deleteMany(query: Query<any>): Promise<any> { return this.Schema.deleteMany(query); }
+  deleteMany(query: Query<Record<string, unknown>>): Promise<any> { return this.Schema.deleteMany(query).lean().exec(); }
 
-  async findOneAndUpdate(conditions: Record<string, unknown>, update: Record<string, unknown>): Promise<any> {
+  async findOneAndUpdate(conditions: Record<string, unknown>, update: Record<string, unknown>): Promise<Document | null> {
     let result;
     try {
       result = await this.Schema.findOneAndUpdate(conditions, update, { new: true }).lean().exec();
@@ -44,13 +45,13 @@ class Facade {
     return result;
   }
 
-  findByIdAndUpdate(id: string, update: Record<string, unknown>): Promise<any> { 
+  findByIdAndUpdate(id: string, update: Record<string, unknown>): Promise<Document | null> { 
     return this.Schema.findByIdAndUpdate(id, update, { new: true }).lean().exec(); 
   }
 
-  findById(id: string): Promise<any> { return this.Schema.findById(id).lean().exec(); }
+  findById(id: string): Promise<Document | null> { return this.Schema.findById(id).lean().exec(); }
 
-  findByIdAndRemove(id: string): Promise<any> { return this.Schema.findByIdAndRemove(id).lean().exec(); }
+  findByIdAndRemove(id: string): Promise<Document | null> { return this.Schema.findByIdAndRemove(id).lean().exec(); }
 }
 
 export default Facade;
