91ae03103eeb47828cf0abc01a86ae4dc48b424b
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-11-11 08:24:45 -0500

    finishes typescript improvements

--- test/jest/user/user-facade.spec.ts
+++ test/jest/user/user-facade.spec.ts
@@ -19,4 +19,8 @@ describe('user-facade', () => {
     bcrypt.hash = jest.fn(() => Promise.reject(new Error('bad')));
     await expect(userFacade.encryptPswd('pw')).rejects.toThrow('bad');
   });
+  it('should wait unit tests finish before exiting', async () => { // eslint-disable-line jest/expect-expect
+    const delay = (ms: any) => new Promise((resolve) => setTimeout(() => resolve(true), ms));
+    await delay(4000);
+  });
 });
