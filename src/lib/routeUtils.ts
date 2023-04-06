627ca92ce0e9e7a54a3c90dd76e6707586d16fb1
Joshua Sherman <joshua.v.sherman@gmail.com>
2023-04-06 08:30:39 -0400

    refactor

--- src/lib/routeUtils.ts
+++ src/lib/routeUtils.ts
@@ -1,17 +1,28 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import AuthUtils from '../auth/authUtils';
 import Controller from './controller';
 
-function setRoot(router: Router, controller: Controller, authUtils: typeof AuthUtils): void {
+const makeAction = (
+  req: Request, 
+  res: Response,
+  method:string,
+  controller: { [x: string]: (req:Request, res:Response)=>Promise<any> }, 
+  authUtils: typeof AuthUtils,
+) => async () => {
+  try {
+    await authUtils.ensureAuthenticated(req);
+    // eslint-disable-next-line security/detect-object-injection
+    await controller[method](req, res);
+  } catch (err) { res.status(401).json({ message: (err as Error).message }); }
+};
+
+function setRoot(router: Router, controller: { [x: string]: (req:Request, res:Response)=>Promise<any> }, authUtils: typeof AuthUtils): void {
   router.route('/')
     .get((req, res) => { (async () => { await controller.find(req, res); })(); })
     .post((req, res) => {
-      (async () => {
-        try {
-          await authUtils.ensureAuthenticated(req);
-          await controller.create(req, res);
-        } catch (err) { res.status(401).json({ message: (err as Error).message }); }
-      })();
+      const action = makeAction(req, res, 'create', controller, authUtils);
+      // eslint-disable-next-line no-void
+      void action();
     })
     .delete((req, res, next) => {
       (async () => {
