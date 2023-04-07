ba2b890ad0af3ca8f82141e355eb992ba701aa90
Joshua Sherman <joshua.v.sherman@gmail.com>
2023-04-07 08:15:18 -0400

    more unit tests

--- src/lib/routeUtils.ts
+++ src/lib/routeUtils.ts
@@ -26,12 +26,9 @@ function setRoot(router: Router, controller: Icontroller, authUtils: typeof Auth
       void action();
     })
     .delete((req, res) => {
-      (async () => {
-        try {
-          await authUtils.ensureAuthenticated(req);
-          await controller.deleteMany(req, res);
-        } catch (err) { res.status(401).json({ message: (err as Error).message }); }
-      })();
+      const action = makeAction(req, res, 'deleteMany', controller, authUtils);
+      // eslint-disable-next-line no-void
+      void action();
     });
 }
 function byId(router: Router, controller: Icontroller, authUtils: typeof AuthUtils): void {
@@ -40,28 +37,16 @@ function byId(router: Router, controller: Icontroller, authUtils: typeof AuthUti
       const action = makeAction(req, res, 'findById', controller, authUtils);
       // eslint-disable-next-line no-void
       void action();
-      // (async () => {
-      //   try {
-      //     await authUtils.ensureAuthenticated(req);
-      //     await controller.findById(req, res);
-      //   } catch (err) { res.status(401).json({ message: (err as Error).message }); }
-      // })();
     })
     .put((req, res) => {
-      (async () => {
-        try {
-          await authUtils.ensureAuthenticated(req);
-          await controller.findByIdAndUpdate(req, res);
-        } catch (err) { res.status(401).json({ message: (err as Error).message }); }
-      })();
+      const action = makeAction(req, res, 'findByIdAndUpdate', controller, authUtils);
+      // eslint-disable-next-line no-void
+      void action();
     })
     .delete((req, res) => {
-      (async () => {
-        try {
-          await authUtils.ensureAuthenticated(req);
-          await controller.findByIdAndRemove(req, res);
-        } catch (err) { res.status(401).json({ message: (err as Error).message }); }
-      })();
+      const action = makeAction(req, res, 'findByIdAndRemove', controller, authUtils);
+      // eslint-disable-next-line no-void
+      void action();
     });
 }
-export default { setRoot, byId };
+export default { setRoot, byId, makeAction };
