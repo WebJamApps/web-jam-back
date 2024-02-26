6ecb693383764a21b6645edce9e5be2a63180388
JoshuaVSherman <joshua.v.sherman@gmail.com>
2024-02-26 18:06:03 -0500

    fixes unit tests

--- src/model/book/book-controller.ts
+++ src/model/book/book-controller.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
 import { Icontroller } from 'src/lib/routeUtils';
-import Controller from '../../lib/controller';
+import Controller from 'src/lib/controller';
 import bookModel from './book-facade';
-import { Icontroller } from '../../lib/routeUtils';
 
 class BookController extends Controller {
   findCheckedOut(req: Request, res: Response) {
--- test/jest/user/user-controller.spec.ts
+++ test/jest/user/user-controller.spec.ts
@@ -1,39 +1,41 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import controller from 'src/model/user/user-controller';
-import Controller from 'src/lib/controller';
+import LibController from 'src/lib/controller';
+import authGoogle from 'src/auth/google';
 
 describe('User Controller', () => {
-  let testObj:any;
+  let status = 0,
+    testObj:any;
   const resStub:any = {
-    status: () => ({ json: (obj: any) => { testObj = obj; } }),
+    status: (s:number) => { status = s; return ({ json: (obj: any) => { testObj = obj; } }); },
   };
   const reqStub:any = { body: { email: '' } };
   it('catches error on findByEmail', async () => {
     controller.resErr = jest.fn();
-    (controller as unknown as Controller).model.findOne = jest.fn(() => Promise.reject(new Error('bad')));
+    (controller as unknown as LibController).model.findOne = jest.fn(() => Promise.reject(new Error('bad')));
     await controller.findByEmail(reqStub, resStub);
     expect(controller.resErr).toHaveBeenCalled();
   });
   it('returns 200 on findByEmail', async () => {
-    (controller as unknown as Controller).model.findOne = jest.fn(() => Promise.resolve({ _id: 'id' }));
+    (controller as unknown as LibController).model.findOne = jest.fn(() => Promise.resolve({ _id: 'id' }));
     await controller.findByEmail(reqStub, resStub);
     expect(testObj._id).toBe('id');
   });
   it('successfully google authenticate', async () => {
-    controller.findOneAndUpdate = jest.fn(() => Promise.resolve({}));
+    (controller as unknown as LibController).model.findOneAndUpdate = jest.fn(() => Promise.resolve({}));
     const authMock:any = jest.fn(() => Promise.resolve({ names: [{ displayName: 'tester' }], emailAddresses: [{ value: 't@s.com' }] }));
-    controller.authGoogle.authenticate = authMock;
+    authGoogle.authenticate = authMock;
     await controller.google({ body: {} } as any, resStub);
     expect(testObj.token).toBeDefined();
     testObj = {};
   });
   it('successfully google authenticate when new user', async () => {
-    controller.create = jest.fn(() => Promise.resolve({ password: 'password' }));
-    controller.findOneAndUpdate = jest.fn(() => Promise.resolve(null));
+    (controller as unknown as LibController).model.create = jest.fn(() => Promise.resolve({ password: 'password' }));
+    (controller as unknown as LibController).model.findOneAndUpdate = jest.fn(() => Promise.resolve(null));
     const authMock:any = jest.fn(() => Promise.resolve({ names: [{ displayName: 'tester' }], emailAddresses: [{ value: 't@s.com' }] }));
-    controller.google.authenticate = authMock;
+    authGoogle.authenticate = authMock;
     await controller.google({ body: {} } as any, resStub);
-    expect(controller.create).toHaveBeenCalled();
+    expect(status).toBe(201);
   }); 
 });
