8be9f14fb7fa0bbb4b7869d9468d065d5fa6da30
JoshuaVSherman <joshua.v.sherman@gmail.com>
2024-02-26 18:15:03 -0500

    fix duplicate import

--- src/model/book/book-controller.ts
+++ src/model/book/book-controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { Icontroller } from 'src/lib/routeUtils';
 import Controller from 'src/lib/controller';
 import bookModel from './book-facade';
-import { Icontroller } from '../../lib/routeUtils';
 
 class BookController extends Controller {
   findCheckedOut(req: Request, res: Response) {
