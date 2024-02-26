025dd6d10ca0f030fa0e7c75c84de291141a1a3c
3-Part-Breath <dogledor@gmail.com>
2024-02-26 18:30:10 -0500

    removed anys from song controller
    add casting to songcontroller calls in src/index

--- src/model/song/song-controller.ts
+++ src/model/song/song-controller.ts
@@ -1,8 +1,9 @@
 import Controller from '../../lib/controller';
 import songModel from './song-facade';
+import { Icontroller } from '../../lib/routeUtils';
 
 class SongController extends Controller {
 
 }
 
-export default new SongController(songModel);
+export default new SongController(songModel) as unknown as Icontroller;
